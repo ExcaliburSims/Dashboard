02b71fc94f3f348ebc16a85cd267cb8487396298
ExcaliburSims <ximeonzilu@gmail.com>
2022-12-13 13:49:59 +0100

    fix: logo component

--- src/layouts/Logo.js
+++ src/layouts/Logo.js
@@ -1,10 +1,11 @@
-import { ReactComponent as LogoDark } from "../assets/images/logos/adminpro.svg";
 import { Link } from "react-router-dom";
+import logo from "../assets/images/logos/logo.png";
 
 const Logo = () => {
   return (
-    <Link to="/">
-      <LogoDark />
+    <Link to="/" className="text-decoration-none">
+      <img src={logo} alt="logo" width={50} height={50} />
+      <span className="title">Bendanga</span>
     </Link>
   );
 };
