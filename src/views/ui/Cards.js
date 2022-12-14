b8ace7c81a31575a59da846ba6f1674c567bbaf3
ExcaliburSims <ximeonzilu@gmail.com>
2022-12-14 01:29:03 +0100

    feat: add map

--- src/views/ui/Cards.js
+++ src/views/ui/Cards.js
@@ -1,5 +1,11 @@
+import { Map, Marker } from "pigeon-maps";
+
 const Cards = () => {
-  return <div></div>;
+  return (
+    <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={100}>
+      <Marker width={50} anchor={[50.879, 4.6997]} />
+    </Map>
+  );
 };
 
 export default Cards;
